a551177f0b1c6beafe9c0977984adf79b3954c0d
luuvmane <162039016+luuvmane@users.noreply.github.com>
2024-03-19 02:08:13 +0300

    Корректировка кода

--- src/utils.py
+++ src/utils.py
@@ -3,11 +3,11 @@ import os
 def get_data():
     """Чтение данных"""
     if os.path.isfile("data/operations.json"):
-        # Код для работы в основном скрипте
+
         with open("data/operations.json", encoding="utf-8") as f:
             return json.load(f)
     else:
-        # Код для работы в тестах
+
         current_dir = os.path.dirname(__file__)
         operations_path = os.path.join(current_dir, "../data/operations.json")
         with open(operations_path, encoding="utf-8") as f:
@@ -16,21 +16,25 @@ def get_data():
 
 
 def filter_data(data):
+
     """Операции с значением ключа"""
     filter_operations = [operation for operation in data if "state" in operation and operation["state"] == "EXECUTED"]
     return filter_operations
 
 def last_five_operations(data):
+
     """Сортировка и вывод последних 5 операций"""
     sorted_operations = sorted(data, key=lambda x: x["date"], reverse=True)
     return sorted_operations[:5]
 
 def format_date(date: str):
+
     """Возвращение строки даты"""
     date_format = date.split("T")[0].split("-")[::-1]
     return ".".join(date_format)
 
 def format_card(card: str):
+
     """Скрытие номера счета"""
     card = card.split()
     card_number = card.pop()
@@ -41,7 +45,9 @@ def format_card(card: str):
         number_secret = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
     return f"{card_name} {number_secret}"
 
+
 def get_data_format(data):
+    """ФОрматирование данных"""
     operations = []
     for operation in data:
         date = format_date(operation["date"])
@@ -52,10 +58,16 @@ def get_data_format(data):
 
         to_formatted = format_card(operation["to"])
         amount = operation["operationAmount"]["amount"]
-        currency = operation["operationAmount"]["currency"]
+        currency_info = operation["operationAmount"]["currency"]
+
+        if isinstance(currency_info, dict):
+            currency_formatted = f"{currency_info['name']} ({currency_info['code']})"
+        else:
+            currency_formatted = str(currency_info)
+
         date_formatted = format_date(operation["date"])
         description = operation["description"]
-        formatted_operation = f"{date_formatted} {description}\n{from_formatted} -> {to_formatted}\n{amount} {currency}\n"
+        formatted_operation = f"{date_formatted} {description}\n{from_formatted} -> {to_formatted}\n{amount} {currency_formatted}\n"
         operations.append(formatted_operation)
 
     return operations
